6061938253bf3d6686d94762cbca2caa6d8f91a7
anxi01 <tjdals2893@naver.com>
2025-08-24 22:42:34 +0900

    새로운 할인 정책 개발 및 테스트

---
+++ src/main/kotlin/hello/core/discount/RateDiscountPolicy.kt
@@ -0,0 +1,23 @@
+package hello.core.discount
+
+import hello.core.member.Grade
+import hello.core.member.Member
+
+class RateDiscountPolicy: DiscountPolicy {
+
+    override fun discount(member: Member, price: Int): Int {
+        return when (member.grade) {
+            Grade.VIP -> {
+                price * DISCOUNT_PERCENT / 100
+            }
+
+            else -> {
+                0
+            }
+        }
+    }
+
+    companion object {
+        const val DISCOUNT_PERCENT = 10
+    }
+}
--- src/main/kotlin/hello/core/order/OrderServiceImpl.kt
+++ src/main/kotlin/hello/core/order/OrderServiceImpl.kt
@@ -1,12 +1,12 @@
 package hello.core.order
 
-import hello.core.discount.FixDiscountPolicy
+import hello.core.discount.RateDiscountPolicy
 import hello.core.member.MemoryMemberRepository
 
 class OrderServiceImpl : OrderService {
 
     private val memberRepository = MemoryMemberRepository()
-    private val discountPolicy = FixDiscountPolicy()
+    private val discountPolicy = RateDiscountPolicy()
 
     override fun createOrder(memberId: Long, itemName: String, itemPrice: Int): Order {
         val member = memberRepository.findById(memberId)
